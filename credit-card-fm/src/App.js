39e4ca0a9364f3db834096851d55c848dc339a32
jcheng1022 <jackycheng1022@gmail.com>
2022-10-24 14:56:19 -0400

    start and add credit card form

---
+++ credit-card-fm/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="cardContainer"></div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ credit-card-fm/src/components/Form.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div className="formContainer">
+      <div className="innerFormContainer">
+        <form>
+          <div className="inputRow">
+            <p className="inputLabel"> Cardholder Name</p>
+            <input
+              className="inputField"
+              type="text"
+              placeholder="e.g. Jane Appleseed"
+            />
+          </div>
+          <div className="inputRow">
+            <p className="inputLabel">Card Number</p>
+            <input
+              className="inputField"
+              type="text"
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
+          </div>
+          <div className="detailRow">
+            <div className="detailSection">
+              <p className="inputLabel"> EXP. DATE (MM/YY)</p>
+              <div className="expDetail">
+                <input
+                  className="expInputField"
+                  type="number"
+                  min="1"
+                  max="12"
+                  placeholder="MM"
+                />
+                <input
+                  className="expInputField"
+                  type="number"
+                  min="1"
+                  max="2022"
+                  placeholder="YY"
+                />
+              </div>
+            </div>
+            <div className="detailSection">
+              <p className="inputLabel"> CVC</p>
+              <input
+                className="inputField"
+                type="number"
+                placeholder="e.g. 123"
+              />
+            </div>
+          </div>
+          <div className="btnContainer">
+            <input className="confirmBtn" type="submit" value="Confirm" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
